909812b887a74ccfeb3e39c581475b3747ac0bbe
zhanglei1032 <zhanglei1032@gmail.com>
2018-01-05 14:07:51 +0800

    添加变位词检测其他解法

--- Chap2/anagram_solution.py
+++ Chap2/anagram_solution.py
@@ -19,4 +19,26 @@ def anagram_solution1(s1, s2):
         pos1 = pos1 + 1
     return still_ok
 
+#解法2：排序后比较字符串
+#解法3：暴力匹配，穷举可能的字符串 性能最差
+#解法4：统计两个单词字符出现次数
+def anagram_solution4(s1, s2):
+    c1 = [0] * 26
+    c2 = [0] * 26
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord("a")
+        c1[pos] = c1[pos] + 1
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] = c2[pos] + 1
+    j = 0
+    still_ok = True
+    while j < 26 and still_ok:
+        if c1[j] == c2[j]:
+            j = j + 1
+        else:
+            still_ok = False
+    return still_ok
+
 print(anagram_solution1("abcd", 'dcba'))
+print(anagram_solution4("abcd", 'dcba'))
