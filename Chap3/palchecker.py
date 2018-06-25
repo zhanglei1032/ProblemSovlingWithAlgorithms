1e4e67b473afa08d43357b09a8362638aadc7e0b
zhanglei1032 <zhanglei1032@gmail.com>
2018-06-25 17:35:59 +0800

    基于双端队列的回文检测

---
+++ Chap3/palchecker.py
@@ -0,0 +1,18 @@
+from Deque import deque
+
+def palchecker(aString):
+	chardeque = Deque()
+	for c in aString:
+		chardeque.addRear(ch)
+
+	stillEqual = True
+	while chardeque.size() > 1 and stillEqual:
+		first = chardeque.removeFront()
+		last = chardeque.removeRear()
+		if first != last:
+			stillEqual = False
+	return stillEqual
+
+
+print(palchecker('lsdkjfskfjkdsl'))
+print(palchecker('radar'))
